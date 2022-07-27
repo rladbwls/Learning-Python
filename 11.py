8adbe4a1aab8646c035802efc8d1c42e30e7a7ed
YJ_Hacker <32039505+rladbwls@users.noreply.github.com>
2022-07-27 22:36:06 +0900

    07/27 updates
    1~15.py update

---
+++ 11.py
@@ -0,0 +1,5 @@
+N, M = input().split()
+N = int(N)
+M = int(M)
+if 1 <= N and M <= 300:
+    print((N*M)-1)
---
+++ 13.py
@@ -0,0 +1,7 @@
+T = input()
+T = int(T)
+for i in range(1, 1+T):
+    A, B= input().split()
+    A = int(A)
+    B = int(B)
+    print(f"Case #{i}: {A} + {B} = {A + B}")
